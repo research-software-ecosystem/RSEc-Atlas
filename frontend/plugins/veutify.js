6bd76e9b524fd05ab852cca065a06fe048fed019
Alireza Heidari <itisalirh@gmail.com>
2025-06-18 23:17:17 +0200

    Remove custom theme colors from Vuetify configuration

--- frontend/plugins/veutify.js
+++ frontend/plugins/veutify.js
@@ -7,18 +7,6 @@ export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     theme: {
       defaultTheme: "light",
-      themes: {
-        light: {
-          colors: {
-            primary: "#1976D2",
-            secondary: "f5f5f5",
-            error: "#FF5252",
-            info: "#2196F3",
-            success: "#4CAF50",
-            warning: "#FFC107",
-          },
-        },
-      },
     },
     components,
     directives,

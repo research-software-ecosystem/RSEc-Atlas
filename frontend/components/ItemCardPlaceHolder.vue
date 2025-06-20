3d0f31bd3afb625c13bc70e6cf57a56de1818400
Alireza Heidari <itisalirh@gmail.com>
2025-06-20 23:53:53 +0200

    Adds placeholder skeleton component for item cards
    Introduces a new Vue component that displays a skeleton loader for item cards.
    The skeleton includes randomized badge sizes and description widths to simulate dynamic content loading.
    Enhances user experience by providing a visual placeholder while data is being fetched or filtered.

---
+++ frontend/components/ItemCardPlaceHolder.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const badges = ["w-12", "w-18", "w-28"].sort(() => Math.random() - 0.5);
+const description = ["w-4/5", "w-3/12", "w-7/12"].sort(
+  () => Math.random() - 0.5,
+);
+</script>
+
+<template>
+  <UCard class="h-64" :ui="{ body: 'flex h-full flex-col justify-between' }">
+    <div>
+      <div class="mb-4">
+        <USkeleton class="h-4 w-48" />
+
+        <div class="flex gap-2">
+          <USkeleton v-for="i in badges" :key="i" class="mt-2 h-4" :class="i" />
+        </div>
+      </div>
+
+      <USkeleton
+        v-for="i in description"
+        :key="i"
+        class="mt-2 h-4"
+        :class="i"
+      />
+    </div>
+
+    <USkeleton class="flex h-8 w-1/5 self-end" />
+  </UCard>
+</template>

a2544ee27323a4b48edad4c6fbad15d6a2923694
Alireza Heidari <itisalirh@gmail.com>
2025-06-21 21:31:13 +0200

    Adds reusable code block component with copy-to-clipboard support
    Introduces a new Vue component for displaying and copying code snippets.
    Supports clipboard functionality with user feedback via toast notifications for success or failure.

---
+++ frontend/components/CodBlock.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+interface Props {
+  code: string;
+}
+
+const props = defineProps<Props>();
+
+const toast = useToast();
+
+function copyToClipboard() {
+  navigator.clipboard
+    .writeText(props.code)
+    .then(() => {
+      toast.add({
+        title: "Code copied to clipboard!",
+        color: "success",
+        icon: "uil:check-circle",
+      });
+    })
+    .catch(() => {
+      toast.add({
+        title: "Failed to copy code",
+        color: "error",
+        icon: "uil:exclamation-triangle",
+      });
+    });
+}
+</script>
+
+<template>
+  <div
+    class="bg-black-800 mb-4 flex justify-between gap-2 rounded-lg border p-2"
+  >
+    <code>
+      <slot>> {{ props.code }}</slot>
+    </code>
+
+    <UTooltip :delay-duration="0" text="Copy to clipboard">
+      <UButton
+        class="h-fit cursor-pointer rounded bg-gray-700 px-2 py-1 text-white hover:bg-gray-600"
+        title="Copy to clipboard"
+        leading-icon="uil:copy"
+        @click="copyToClipboard"
+      />
+    </UTooltip>
+  </div>
+</template>

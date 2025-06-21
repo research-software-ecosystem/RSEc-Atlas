c9e6e9640e624b84ec02b42af87fb08200c7fbea
Alireza Heidari <itisalirh@gmail.com>
2025-06-21 21:30:31 +0200

    Enhances header and footer responsiveness and styling
    Improves the header's layout by making it sticky, adding a shadow, and refining dark mode support. Updates footer text to include responsive font sizes and interactive links to the repository.

--- frontend/app.vue
+++ frontend/app.vue
@@ -18,10 +18,12 @@ const isDark = computed({
   />
 
   <UApp class="bg-chicago-50 flex h-dvh flex-col font-sans">
-    <header class="px-6 py-2">
+    <header
+      class="sticky top-0 z-50 bg-white px-6 py-2 shadow-sm dark:bg-gray-900"
+    >
       <nav>
-        <div class="flex justify-between">
-          <NuxtLink to="/" class="flex items-center gap-2 text-2xl font-bold">
+        <div class="flex flex-col justify-between md:flex-row">
+          <NuxtLink to="/" class="flex gap-2 text-2xl font-bold">
             <img
               class="h-10 w-auto"
               alt="RSE Logo"
@@ -31,7 +33,7 @@ const isDark = computed({
             <span>RSE Metadata Explorer</span>
           </NuxtLink>
 
-          <div class="flex items-center gap-2">
+          <div class="flex items-center justify-end gap-2">
             <ClientOnly v-if="!colorMode?.forced" class="cursor-pointer">
               <UButton
                 :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
@@ -49,7 +51,7 @@ const isDark = computed({
               target="_blank"
               role="button"
               to="https://github.com/research-software-ecosystem/research-software-ecosystem.github.io"
-              class="inline-flex cursor-pointer items-center rounded-lg bg-[#24292F] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-[#24292F]/90 focus:ring-4 focus:ring-[#24292F]/50 focus:outline-none dark:hover:bg-[#050708]/30 dark:focus:ring-gray-500"
+              class="inline-flex cursor-pointer items-center rounded-lg border border-gray-200 bg-[#24292F] px-5 py-2.5 text-center text-sm font-medium text-white transition-all duration-300 hover:bg-gray-500 hover:text-white hover:shadow-lg focus:ring-4 focus:ring-[#24292F]/50 focus:outline-none dark:border-gray-700 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-500"
             >
               <Icon name="uil-github" class="me-2 h-4 w-4" />
 
@@ -68,9 +70,18 @@ const isDark = computed({
       class="fixed bottom-0 w-full bg-gray-100 p-3 shadow-amber-50 dark:bg-gray-800"
     >
       <div>
-        <p class="text-center text-sm text-gray-500 dark:text-gray-400">
+        <p
+          class="text-center text-xs text-gray-500 md:text-sm dark:text-gray-400"
+        >
           A webapp to browse through all the bio tools and containers in the
-          <b>Research Software Ecosystem</b> repository.
+          <NuxtLink
+            target="_blank"
+            class="font-bold transition-all hover:text-gray-700 dark:hover:text-gray-300"
+            to="https://github.com/research-software-ecosystem/research-software-ecosystem.github.io?tab=readme-ov-file"
+          >
+            Research Software Ecosystem
+          </NuxtLink>
+          repository.
         </p>
       </div>
     </footer>

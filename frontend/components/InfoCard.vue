6782a64626c87ef2de2d792b20a947c050185d1f
Alireza Heidari <itisalirh@gmail.com>
2025-06-21 21:32:04 +0200

    Add reusable InfoCard component with title and content slots
    Introduces a new InfoCard component to enhance UI modularity and reusability.
    The component supports an optional title with an
    icon and utilizes named slots for flexible content injection, improving customization and consistency across the application.

---
+++ frontend/components/InfoCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  title?: string;
+  titleIcon?: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <UCard class="mb-2">
+    <h3 class="mb-2 flex items-center text-lg font-semibold">
+      <Icon
+        v-if="props.titleIcon"
+        :name="props.titleIcon"
+        class="mr-2 text-lg"
+      />
+
+      <slot name="title">
+        {{ props.title }}
+      </slot>
+    </h3>
+
+    <slot name="content">
+      <div class="flex flex-wrap gap-2">
+        <slot />
+      </div>
+    </slot>
+  </UCard>
+</template>

f0a986cb44c8bc00fd2de46dd46c229bf613635b
Alireza Heidari <8046843+itisAliRH@users.noreply.github.com>
2026-06-18 14:20:06 +0200

    chore: explicitly configure monaco-editor workers in nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -38,7 +38,33 @@ export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
 
   vite: {
-    plugins: [monacoEditorPlugin()],
+    plugins: [
+      monacoEditorPlugin({
+        languageWorkers: [],
+        customWorkers: [
+          {
+            label: "editorWorkerService",
+            entry: "monaco-editor/esm/vs/editor/editor.worker.js",
+          },
+          {
+            label: "css",
+            entry: "monaco-editor/esm/vs/language/css/css.worker.js",
+          },
+          {
+            label: "html",
+            entry: "monaco-editor/esm/vs/language/html/html.worker.js",
+          },
+          {
+            label: "json",
+            entry: "monaco-editor/esm/vs/language/json/json.worker.js",
+          },
+          {
+            label: "typescript",
+            entry: "monaco-editor/esm/vs/language/typescript/ts.worker.js",
+          },
+        ],
+      }),
+    ],
     optimizeDeps: {
       include: ["monaco-editor"],
     },
